ec665921a685e2dc721911bc8f9239d582e94c0d
George <starcalluh@live.com>
2019-10-02 18:34:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
 def reverse_each_word(string)
- string.each { |words|  puts string.reverse }
+ new_array = string.split(" ")
+ array = []
+ new_array.each {|string|
+   array.push(string.reverse)
+ }
 end
